e86addc52065c12358373548187024c9ace9dcaa
Natali Vilatuña <natalievc39@gmail.com>
2024-09-12 21:49:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function efectoHabilidades(){
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
+        habilidades[0].classList.add("matlab");
         habilidades[1].classList.add("htmlcss");
         habilidades[2].classList.add("photoshop");
         habilidades[3].classList.add("wordpress");
