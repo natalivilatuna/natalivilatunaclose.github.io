25a63f478a3465b87f7c72b9dc55fa6cf90e98a7
Natali Vilatuña <natalievc39@gmail.com>
2024-09-12 22:27:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -37,20 +37,11 @@ function efectoHabilidades(){
         habilidades[13].classList.add("creatividad");
         habilidades[14].classList.add("dedicacion");
         habilidades[15].classList.add("proyect");
-    }
-}
-function efectoHabilidades(){
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >= 300){
-        let habilidades = document.getElementsByClassName("progreso");
-        // ... (tus habilidades existentes)
         habilidades[16].classList.add("ingles");
         habilidades[17].classList.add("espanol");
         habilidades[18].classList.add("frances");
     }
 }
-
 
 //detecto el scrolling para aplicar la animacion de la barra de habilidades
 window.onscroll = function(){
